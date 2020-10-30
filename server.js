359a27266d5ce41e71bc8094aeeeff0e561eb1b8
RJ Wright <rj@rjwright.me>
2020-10-30 21:36:45 +1100

    Update route function for handling POST requests when a new note is saved

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 var path = require("path");
 
+let lastNoteId;
+
 // Set up the Express App
 // =============================================================
 var app = express();
@@ -30,8 +32,23 @@ app.get("/api/notes", function(req, res) {
     });
 });
 app.post("/api/notes", function(req, res) {
-    console.log(req.data);
-    res = true;
+    // Read in JSON DB to see what the last note ID is
+    const obj = JSON.parse(fs.readFileSync(path.join(__dirname, "db/db.json"), 'utf8'));
+    
+    // Assign the ID of the previous note
+    lastNoteId = obj[0].id;
+
+    // If there was no previous note or ID, assign an ID of 0.
+    if (isNaN(lastNoteId)) {
+        lastNoteId = 0;
+    }
+
+    lastNoteId++;
+    
+    // Add the new ID to new note object
+    req.body.id = lastNoteId;
+
+    res.send(req.body);
 });
 // app.delete("/api/notes/:id", function(req, res) {
 //     res.sendFile(path.join(__dirname, "public/notes.html"));
